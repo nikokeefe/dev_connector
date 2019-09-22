3a84d0edb539643adb29d45f18d34e7aea63131a
Nik O'Keefe <nikwillok@gmail.com>
2019-09-22 13:58:25 +1200

    removed initial state from alert.js.

---
+++ client/build/precache-manifest.1105f3ab067e24915ace10927759028e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927f710cbfa0c7dcc88815bd291a200d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3ab745ba64ca66c471",
+    "url": "/static/css/main.17e6f2ec.chunk.css"
+  },
+  {
+    "revision": "5b50a23699437ff5d8c3",
+    "url": "/static/js/2.429dc21e.chunk.js"
+  },
+  {
+    "revision": "cd3ab745ba64ca66c471",
+    "url": "/static/js/main.04473941.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
